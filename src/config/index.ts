350b8593adf42c00351cca8b41cb91f10a41d4b7
nihalDelimp <nihal@delimp.com>
2022-11-02 12:57:40 +0530

    Config path change

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,6 @@
 import { config } from 'dotenv';
 import Shopify from 'shopify-api-node';
-config({ path: `.env.${process.env.NODE_ENV || 'development'}.local` });
+config({ path: `.env` });
 
 export const CREDENTIALS = process.env.CREDENTIALS === 'true';
 export const { API_KEY, HOST_SCHEME, HOST, API_SECRET_KEY, SHOPIFY_PASSWORD, SHOPIFY_SHOP_NAME, NODE_ENV, PORT, DB_HOST, DB_PORT, DB_DATABASE, SECRET_KEY, LOG_FORMAT, LOG_DIR, ORIGIN, STRIPE_SECRET, STRIP_COUNTRY, STRIPE_APPLICATION_FEE, STRIPE_CURRENCY, STRIPE_REFRESH_URL, STRIPE_RETURN_URL } = process.env;
