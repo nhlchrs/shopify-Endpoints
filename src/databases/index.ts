8f1ee1ba27643c1b49c99457e66215f93e1dcd4d
nihalDelimp <nihal@delimp.com>
2022-11-25 13:09:51 +0530

    New changaes regarding database

--- src/databases/index.ts
+++ src/databases/index.ts
@@ -1,7 +1,8 @@
 import { DB_DATABASE, DB_HOST, DB_PORT } from '@config';
 
 export const dbConnection = {
-  url: `mongodb+srv://${DB_HOST}/${DB_DATABASE}`,
+  // url: `mongodb+srv://${DB_HOST}/${DB_DATABASE}`,
+  url:"mongodb+srv://admin:dIYgKjiImtp9JsDC@cluster0.qixroy4.mongodb.net/shopping_cart",
   options: {
 
   },
