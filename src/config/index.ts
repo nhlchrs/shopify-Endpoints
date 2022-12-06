eac0cdc98391b5fdb69eb1d5a82c8faaf198f204
nihalDelimp <nihal@delimp.com>
2022-12-06 14:45:22 +0530

    Env chnages for Developemnt or production

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,7 +1,6 @@
 import { config } from 'dotenv';
 import Shopify from 'shopify-api-node';
-config({ path: `.env.development.local` });
-// config({ path: `.env.${process.env.NODE_ENV || 'development'}.local` });
+config({ path: `.env.${process.env.NODE_ENV || 'development'}.local` });
 
 export const CREDENTIALS = process.env.CREDENTIALS === 'true';
 export const { API_KEY, HOST_SCHEME, HOST, API_SECRET_KEY, SHOPIFY_PASSWORD, SHOPIFY_SHOP_NAME, NODE_ENV, PORT, DB_HOST, DB_PORT, DB_DATABASE, SECRET_KEY, LOG_FORMAT, LOG_DIR, ORIGIN } = process.env;
